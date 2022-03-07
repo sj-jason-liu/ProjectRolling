f4a18e93e6758bb7f7b01b5eef8350f1a4f5a957
sj-jason-liu <sj.jason.liu@gmail.com>
2022-03-07 20:37:27 +0800

    Fixed NavMeshAgent chasing error.

---
+++ Assets/Scripts/Somebody.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Somebody : MonoBehaviour
+{
+    private NavMeshAgent _navMeshAgent;
+
+    [SerializeField]
+    private Transform _target;
+
+    private Vector3 _originPosition;
+
+    private CharacterController _controller;
+
+    private bool _isSomebodyNoticed;
+    
+    //somebody should curious about shooting scene
+    //or walkby close to the shooting scene
+    //when player walk toward them
+    //they will walk away immediately
+
+    //some moving patterns
+    //A: try to get close to shooting scene
+    //B: Walkby shooting scene
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        if(_navMeshAgent == null)
+        {
+            Debug.LogError("NavMeshAgent is NULL!");
+        }
+        _originPosition = transform.position;
+        Debug.Log("Somebody is ready!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            _isSomebodyNoticed = !_isSomebodyNoticed;
+        }
+        
+        switch(_isSomebodyNoticed)
+        {
+            case true:
+                _navMeshAgent.destination = _originPosition;
+                
+                break;
+            case false:
+                _navMeshAgent.destination = _target.position;
+                break;
+        }
+    }
+}
